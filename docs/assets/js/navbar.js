c55c5462941039c657678da576357dd71dbb549a
paviag <98894987+paviag@users.noreply.github.com>
2024-02-29 20:00:08 -0500

    Create navbar.js

---
+++ docs/assets/js/navbar.js
@@ -0,0 +1,11 @@
+function openNav() {
+    sn = document.getElementById("side-nav");
+    sn.style.width = "100%";
+    sn.style.padding = "20px";
+}
+
+function closeNav() {
+    sn = document.getElementById("side-nav");
+    sn.style.width = "0";
+    sn.style.padding = "0";
+}
